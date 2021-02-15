c86ef3fa400796303305625ca3967dd7513ee634
thallesyasmim <thallesgabriel1307@gmail.com>
2021-02-15 08:32:03 -0300

    Remove migration change table name

--- db/migrations/20191120191110-users-rename-table.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  up: (queryInterface, _Sequelize) =>
-    queryInterface.renameTable('Users', 'users'),
-  down: (queryInterface, _Sequelize) =>
-    queryInterface.renameTable('users', 'Users'),
-}
