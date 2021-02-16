06a059212635b9ca09c1a27e4c3ef595f2617d12
thallesyasmim <thallesgabriel1307@gmail.com>
2021-02-16 20:56:28 -0300

    Generate token

--- src/services/token.js
+++ src/services/token.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken')
 
-const sign = payload => jwt.sign() // jwt.decode() - return information of payload | jwt.sign() - authenticate | jwt.verify() - 
+const sign = payload => jwt.sign(payload, 'yasmim') // jwt.decode() - return information of payload | jwt.sign() - generate new jwt | jwt.verify() - verify signature and return information payload
+// yasmim - secret
 
 const verify = _ => new Promise((resolve, reject) => resolve(true))
 
