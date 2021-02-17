94277d40b3232eaa2b57df262d2e28f8f44aa346
Thalles Gabriel <thallesgabriel1307@gmail.com>
2021-02-17 15:54:34 -0300

    Generating jwt with a private key and new algorithm

--- src/services/token.js
+++ src/services/token.js
@@ -1,6 +1,14 @@
 const jwt = require('jsonwebtoken')
 
-const sign = payload => jwt.sign(payload, 'yasmim') // jwt.decode() - return information of payload | jwt.sign() - generate new jwt | jwt.verify() - verify signature and return information payload
+const { crypto: config } = require('../../config')
+
+const signOptions = { // If we use the same secret to generate and validate the token, people who discover our secret can use it to enter services that they shouldn't
+  algorithm: 'RS256' // There are different algorithms for private keys, let's use RS256 for example
+}
+// So we will use a private key to generate new tokens and a public one to validate
+
+
+const sign = payload => jwt.sign(payload, config.jwt.privateKey, signOptions) // jwt.decode() - return information of payload | jwt.sign() - generate new jwt | jwt.verify() - verify signature and return information payload
 // yasmim - secret
 
 const verify = token => new Promise((resolve, reject) => {
