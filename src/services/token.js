3f1c07c6181123806532649bdb18bc283aa1bda9
Thalles Gabriel <thallesgabriel1307@gmail.com>
2021-02-17 17:42:07 -0300

    Adding the private key to generate new jwt and the public key to verify the jwt

--- src/services/token.js
+++ src/services/token.js
@@ -1,5 +1,5 @@
+require('dotenv/config')
 const jwt = require('jsonwebtoken')
-
 const { crypto: config } = require('../../config')
 
 const signOptions = { // If we use the same secret to generate and validate the token, people who discover our secret can use it to enter services that they shouldn't
@@ -7,12 +7,11 @@ const signOptions = { // If we use the same secret to generate and validate the 
 }
 // So we will use a private key to generate new tokens and a public one to validate
 
-
-const sign = payload => jwt.sign(payload, config.jwt.privateKey, signOptions) // jwt.decode() - return information of payload | jwt.sign() - generate new jwt | jwt.verify() - verify signature and return information payload
+const sign = payload => jwt.sign(payload, config.jwt.privateKey.replace(/\n/gm, '\n'), signOptions) // jwt.decode() - return information of payload | jwt.sign() - generate new jwt | jwt.verify() - verify signature and return information payload
 // yasmim - secret
 
 const verify = token => new Promise((resolve, reject) => {
-  jwt.verify(token, 'yasmim', (error, data) => error ? reject(error) : resolve(data)) // Many people use decode() at this stage of verification, but this is wrong. Because regardless of the secret, authentication would be performed, which is wrong, we should use verify
+  jwt.verify(token, config.jwt.privatePublic.replace(/\n/gm, '\n'), (error, data) => error ? reject(error) : resolve(data)) // Many people use decode() at this stage of verification, but this is wrong. Because regardless of the secret, authentication would be performed, which is wrong, we should use verify
 })
 
 module.exports = {
