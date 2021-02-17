8e623d778e5fb58d8fd16d70879714af0d460cf8
thallesyasmim <thallesgabriel1307@gmail.com>
2021-02-16 21:05:43 -0300

    Verify token signature

--- src/services/token.js
+++ src/services/token.js
@@ -3,7 +3,9 @@ const jwt = require('jsonwebtoken')
 const sign = payload => jwt.sign(payload, 'yasmim') // jwt.decode() - return information of payload | jwt.sign() - generate new jwt | jwt.verify() - verify signature and return information payload
 // yasmim - secret
 
-const verify = _ => new Promise((resolve, reject) => resolve(true))
+const verify = token => new Promise((resolve, reject) => {
+  jwt.verify(token, 'yasmim', (error, data) => error ? reject(error) : resolve(data)) // Many people use decode() at this stage of verification, but this is wrong. Because regardless of the secret, authentication would be performed, which is wrong, we should use verify
+})
 
 module.exports = {
   sign,
